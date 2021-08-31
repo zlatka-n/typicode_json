7b274f0fa0154a2c7ba55ad134ae059227a1fb5b
zlatka-n <zla.b.ngu@gmail.com>
2021-08-31 20:59:47 +0200

    header and footer added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import MainPage from "./components/MainPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./css/index.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <MainPage />
+      <Footer />
     </div>
   );
 }
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -69,7 +69,7 @@ function MainPage() {
     setResults(filterUser);
   };
 
-  //user selects alphabeticall sorting
+  //user selects alphabetical sorting
   const getSortSelection = () => {
     if (results.length !== 0) {
       return (
